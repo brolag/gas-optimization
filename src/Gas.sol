76afedb419b3f985766e43893cc855b03266530b
Alfredo Bonilla <info@alfredobonilla.com>
2024-09-09 11:08:14 -0600

    feat: add more optimizations

--- src/Gas.sol
+++ src/Gas.sol
@@ -9,20 +9,24 @@ contract GasContract {
     mapping(address => uint256) private addrToAmount;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
+
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) payable {
         contractOwner = msg.sender;
         assembly {
+            // Calculate the storage slot for balances[msg.sender]
             mstore(0x0, caller())
             mstore(0x20, balances.slot)
             let balancesSlot := keccak256(0x0, 0x40)
 
+            // Store _totalSupply in the calculated storage slot
             sstore(balancesSlot, _totalSupply)
 
-            let adminsPtr := add(_admins, 0x20)
+            // Loop through _admins and set administrators
             for { let i := 0 } lt(i, 5) { i := add(i, 1) } {
-                sstore(add(administrators.slot, i), mload(add(adminsPtr, mul(i, 0x20))))
+                let admin := mload(add(add(_admins, 0x20), mul(i, 0x20)))
+                sstore(add(administrators.slot, i), admin)
             }
         }
     }
@@ -46,10 +50,10 @@ contract GasContract {
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) external {
-        if (_tier >= 255) {
+        if (msg.sender != contractOwner) {
             revert();
         }
-        if (msg.sender != contractOwner) {
+        if (_tier >= 255) {
             revert();
         }
         whitelist[_userAddrs] = _tier < 3 ? 2 : 3;
